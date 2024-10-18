5447f8a983c3df474c17254ed062b7f9d0bd608f
Murat S. <muratsutyemez@outlook.com>
2024-10-18 23:30:06 +0300

    Future nesnesi eklendi asyc await temelleri

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+     // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+          '/': (context) => MyHomePage(title: 'Flutter Demo Home Page'),
+          '/settings' : (context) => SettingsPage(),
+      },
     );
   }
 }
@@ -37,7 +41,9 @@ class _MyHomePageState extends State<MyHomePage> {
   _MyHomePageState() {
     print("myHomepage");
   }
-  int aktifbuton = 0 ;
+
+  int aktifbuton = 0;
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -46,181 +52,361 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-  bool check = false ;
+
+  bool check = false;
+
   //String text = '';
   String istenenYazi = 'İİ';
 
-  int sinif = 4 ;
+  int sinif = 4;
+
   String baslik = 'ogrenciler';
-  List<String> ogrenciler = ['Ali','Ayşe','Can'];
+  List<String> ogrenciler = ['Ali', 'Ayşe', 'Can'];
   TextEditingController _controller = TextEditingController();
   var ogrenciEkle;
+
+  void yeniOgrEkle(String yeniOgr){
+
+    setState(() {
+      ogrenciler = [...ogrenciler,yeniOgr];
+
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     print('MyhomePageState çalıştıii ');
 
     return Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.menu),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(
-            'MeetUp',
-            textAlign: TextAlign.center, // Düzeltilmiş kısım
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.add),
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(
+          'MeetUp',
+          textAlign: TextAlign.center, // Düzeltilmiş kısım
+        ),
+        actions: [
+          IconButton(
               onPressed: () {
-                print('eklendi');
+              Navigator.of(context).pushNamed('/settings');
               },
-            ),
-            Icon(Icons.remove),
+              icon: Icon(Icons.settings),
+          ),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              print('eklendi');
+            },
+          ),
+          Icon(Icons.remove),
+        ],
+      ),
+      body: sinifBilgisi(
+        sinif: sinif,
+        baslik: baslik,
+        ogrenciler: ogrenciler,
+        yeniOgrEkle : yeniOgrEkle,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+              arkaPlan(),
+              Positioned(
+                  top: 100,
+                  left:150,
+                  right: 150,
+                  child: LayoutBuilder(
+                    builder: (context , constraint){
+                      if(constraint.maxWidth > 150){
+                        return Row(
+                          children: [
+                          ogr(),
+                            Expanded(child: Text("Seçildi")),
+                          ],
+                        );
+                      }else{
+                        return ogr();
+                      }
+
+
+                    }
+                  ),
+              ),
+              Positioned(
+                bottom: 50,
+                left:30,
+                right: 30,
+                child: ogrEkle(),
+
+              ),
+
+
+
           ],
         ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
+      ),
+    );
+  }
+}
 
-          child: Column(
+class sinifBilgisi extends InheritedWidget {
+  const sinifBilgisi({
+    super.key,
+    required this.sinif,
+    required this.baslik,
+    required this.ogrenciler,
+    required this.yeniOgrEkle,
+    required Widget child,
+  }) : super(child: child);
 
-            //mainAxisSize: MainAxisSize.min,
+  final int sinif;
+  final String baslik;
+  final List<String> ogrenciler;
+  final void Function(String yeniOgr) yeniOgrEkle;
 
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.star
-                  ),
-                  Text(
-                    '$sinif',
-                    textScaleFactor: 2,
-                  ),
-                  Icon(Icons.star
-                  ),
-                ],
+
+  static sinifBilgisi of(BuildContext context) {
+    final sinifBilgisi? result = context.dependOnInheritedWidgetOfExactType<sinifBilgisi>();
+    assert(result != null, 'No sinifBilgisi found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(sinifBilgisi old) {
+    return sinif != old.sinif ||
+        baslik != old.baslik ||
+        ogrenciler != old.ogrenciler ||
+        yeniOgrEkle != old.yeniOgrEkle;
+  }
+}
+
+class ogr extends StatelessWidget {
+  const ogr({
+    super.key,
+
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    final sinifbilgisi = sinifBilgisi.of(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.star),
+            Text(
+              '${sinifbilgisi.sinif}',
+              textScaleFactor: 2,
+            ),
+            Icon(Icons.star),
+          ],
+        ),
+        Text(
+          '${sinifbilgisi.baslik}',
+          textScaleFactor: 1.5,
+        ),
+        ogrList(),
+        ElevatedButton(
+            onPressed: ()  {
+              // final cevap = await Navigator.of(context).push<bool>(MaterialPageRoute(
+               Future<bool?> pushFeture =  Navigator.of(context).push<bool>(MaterialPageRoute(
+                 builder: (context) {
+                   return Ekran2("Beğendiniz mi");
+                 },
+               )
+               );
+               pushFeture.then((bool? x) => {
+               if(x == true){
+               print("Beğendiiiii"),
+               //throw "hata"
+                  Future.value(true)
+               }
+               else{
+                   Navigator.of(context).push<bool>(
+                   MaterialPageRoute(
+                     builder: (context) {
+                       return Ekran2(
+                           " Keşke beğenseydiniz yeaaaaağ Beğendiniz mi");
+                     },
+                   )
+               )
+            }}).then((x) => {
+              if(x == true){
+                print("Beğendiniz.")
+              }
+            }).onError((error, stackTrace) => {
+
+                             }).whenComplete(() => {print("işler biitttii") });
+
+               print("Yapılacak işlere devam ediyorummmmm");
+            },
+            child: Text("Yeni sayfa"))
+      ],
+    );
+  }
+}
+
+class Ekran2 extends StatelessWidget {
+  final String mesaj;
+
+  const Ekran2(this.mesaj, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        print("pop edecek");
+
+        return true;
+
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+
+          body: Column(
+            children: [
+
+              Placeholder(
+                fallbackHeight: 150,
+                fallbackWidth: 75,
               ),
-              Text(
-                '$baslik',
-                textScaleFactor: 1.5,
-              ),
-              for (final o in ogrenciler)
-                Text(o),
-              TextField(
-                controller: _controller,
-                onChanged: (value){
-                  setState(() {
-                    ogrenciEkle = value;
-                  });
 
+              Text(mesaj),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).maybePop(true);
                 },
-              ),
-              ElevatedButton(onPressed: () {
-                  setState(() {
-                    ogrenciler.add(ogrenciEkle);
-                  });
-              },
-              child:
-              Text('Ekle'),
-              ),
+                child: Text("Evet"
+                ),
 
-              yaziYeri(
-                  istenenYazi: istenenYazi,
 
-              ),
-              Text('Yazı Şu şekilde: ${istenenYazi}'),
-
-              Checkbox(
-                  value: check,
-                  onChanged: (value) {
-                    print(value);
-                    setState(() {
-                      if (value != null) {
-                        check = value;
-                      }
-                    });
-                  }),
-              ElevatedButton(
-                  onPressed: aktifbuton == 0
-                      ? () {
-                          setState(() {
-                            aktifbuton = 1;
-                            istenenYazi = 'sıfır';
-                          });
-                          print("0");
-                        }
-                      : null,
-                  child: Text('0')),
-              ElevatedButton(
-                  onPressed: aktifbuton == 1 ?() {
-
-                    print("1");
-                    setState(() {
-                      aktifbuton = 0;
-                      istenenYazi = 'bir';
-                    });
-                  } : null ,
-                  child:
-                  Text('1')
-              ),
-
-              const Text(
-                'You have pushed the button this many times: merhabaaa',
-                style: TextStyle(color: Colors.blue),
-              ),
-              Text(
-                '${_counter}',
-                style: Theme.of(context).textTheme.headlineMedium,
               ),
               ElevatedButton(
                 onPressed: () {
-                  _incrementCounter();
-                  print("Artııııı");
+                  Navigator.of(context).maybePop(false);
                 },
-                child: Text(
-                  "Increment",
+                child: Text("Hayır "
                 ),
-              ),
-              yazi('şuanki değer  $_counter'),
-              sayac(
-                d: 'sayac deger dışarı $_counter',
-                ilk: 2,
+
               ),
             ],
           ),
+      ),
+    );
+  }
+}
+
+class ogrEkle extends StatefulWidget {
+
+  const ogrEkle({
+    super.key,
+  });
+
+  @override
+  State<ogrEkle> createState() => _ogrEkleState();
+}
+
+class _ogrEkleState extends State<ogrEkle> {
+  final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final sinifbilgisi = sinifBilgisi.of(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextField(
+          controller: _controller,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Ekle'),
-            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Çıkar'),
-          ],
-        ));
+        Align(
+          alignment: Alignment.bottomRight ,
+          child: ElevatedButton(
+            onPressed: () {
+              final yeniOgr = _controller.text;
+              sinifbilgisi.yeniOgrEkle(yeniOgr);
+              _controller.text = "";
+            },
+            child: Text('Ekle'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class arkaPlan extends StatelessWidget {
+  const arkaPlan({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 175,
+      color: Colors.deepOrange,
+      child: Container(
+        color: Colors.deepPurple,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 60,
+            height: 100,
+            color: Colors.amber,
+            child: Text(
+                'Merhoba',
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ogrList extends StatelessWidget {
+  const ogrList({
+    super.key,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    final sinifbilgisi = sinifBilgisi.of(context);
+    return Column(
+      children: [
+        for (final o in sinifbilgisi.ogrenciler)
+          Text(o
+          ),
+      ],
+    );
   }
 }
 
 class yaziYeri extends StatefulWidget {
   final String istenenYazi;
 
-  const yaziYeri({
-    super.key, required this.istenenYazi
-  });
-
+  const yaziYeri({super.key, required this.istenenYazi});
 
   @override
   State<yaziYeri> createState() => _yaziYeriState();
 }
 
 class _yaziYeriState extends State<yaziYeri> {
-
   late TextEditingController controller;
 
   @override
@@ -229,41 +415,39 @@ class _yaziYeriState extends State<yaziYeri> {
     controller = TextEditingController();
     controller.addListener(() {
       print('$controller');
-
     });
   }
+
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
+
   @override
   void didUpdateWidget(covariant yaziYeri oldWidget) {
     // TODO: implement didUpdateWidget
     super.didUpdateWidget(oldWidget);
 
-    if(oldWidget.istenenYazi != widget.istenenYazi){
+    if (oldWidget.istenenYazi != widget.istenenYazi) {
       controller.text = widget.istenenYazi;
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller ,
+      controller: controller,
       onChanged: (value) {
         print(value);
-
       },
       decoration: InputDecoration(
-        suffixIcon: IconButton(
-          icon: Icon(Icons.delete),
-          onPressed: () {
-            controller.text = '';
-          },
-        )
-      ),
+          suffixIcon: IconButton(
+        icon: Icon(Icons.delete),
+        onPressed: () {
+          controller.text = '';
+        },
+      )),
     );
   }
 }
@@ -308,6 +492,29 @@ class _sayacState extends State<sayac> {
         });
       },
       child: Text('${widget.d} $sayi'),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Settings bar page"),
+      ),
+      body: Center(
+        child: Text("Settings page "),
+      ),
+
     );
   }
 }
